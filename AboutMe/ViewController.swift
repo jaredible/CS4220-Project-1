4a0a8c98e9b4d2cd8223a349d1081601bf4286b9
Jaredible <jareddiehl96@gmail.com>
2020-06-22 22:34:29 -0500

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Jared Diehl on 6/22/20.
+//  Copyright © 2020 umsl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
