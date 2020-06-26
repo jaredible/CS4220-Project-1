b87e403ff6dfcc623660bcb3356a8765f5128d4c
Jaredible <jareddiehl96@gmail.com>
2020-06-25 19:48:42 -0500

    Completed requirements

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    private var gradient: CAGradientLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        gradient = CAGradientLayer()
+        gradient.frame = profilePicture.bounds
+        gradient.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.cgColor,
+            UIColor.black.cgColor,
+            UIColor.clear.cgColor
+        ]
+        gradient.locations = [0.1, 0.2, 0.6, 0.9]
+        profilePicture.layer.mask = gradient
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        gradient.frame = profilePicture.bounds
     }
 
-
 }
-
---
+++ EXTRACREDIT.md
@@ -0,0 +1,5 @@
+# Extra Credit
+
+- background-color of #DBD3DC
+- transparent gradient mask over the profile image
+- centered text
